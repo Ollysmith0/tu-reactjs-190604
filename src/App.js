b1245076fa416b00e39af7c7eb071a70ff2d5e2f
Tu <Ollysmith0@gmail.com>
2019-08-29 21:32:02 +0700

    keke

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(props) {
   return (
-    <div className="abc">
-      <header className="App-header">
-        <p>
-          Nordic coder
-        </p>
-      </header>
+    <div>
+      {props.dssp.map(sp =>{
+        return (
+          <div className="product">
+            <img className="product-img" src={sp.imgURL} />
+            <p>{sp.title}</p>
+            <p>{sp.description}</p>
+            <p>{sp.price}</p>
+          </div>
+        )
+      })}
     </div>
   );
 }
