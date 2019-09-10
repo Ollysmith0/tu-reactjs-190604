dd15b719f6256a1ef3ae8ff5e75ffd7992d57486
Tu <Ollysmith0@gmail.com>
2019-09-10 20:00:14 +0700

    sort

--- src/App.js
+++ src/App.js
@@ -4,48 +4,51 @@ import "./App.css";
 import Footer from "./components/Footer/Footer";
 import ProductList from "./components/ProductList/ProductList";
 import Login from "./components/Login/Login";
-import Register from "./components/Register/Register"
+import ProductItem from "./components/ProductItem/ProductItem";
+
+
 
 const dssp = [
   {
     productId: 1,
     shop: "NC",
-    name: "Tui xach hang hieu",
-    price: 50000,
+    name: "A",
+    price: 500001,
     pricePromoted: 40000
   },
   {
     productId: 2,
     shop: "NC",
-    name: "Tui xach hang hieu",
-    price: 50000,
+    name: "B",
+    price: 500007,
     pricePromoted: 40000
   },
   {
     productId: 3,
     shop: "NC",
-    name: "Tui xach hang hieu",
-    price: 50000,
+    name: "C",
+    price: 500005,
     pricePromoted: 40000
   },
   {
     productId: 4,
     shop: "NC",
-    name: "Tui xach hang hieu",
-    price: 50000,
+    name: "D",
+    price: 500009,
     pricePromoted: 40000
   },
   {
     productId: 5,
     shop: "NC",
-    name: "Tui xach hang hieu",
-    price: 50000,
+    name: "E",
+    price: 500012,
     pricePromoted: 40000
   }
 ];
 
 function App() {
   const [prdInCart, setPrdInCart] = useState([])
+  const [data, setData] = useState(dssp)
   const addToCart = (product) => {
     setPrdInCart([...prdInCart, product])
   }
@@ -54,15 +57,40 @@ function App() {
     const products = prdInCart.filter(elm => elm.productId !== product.productId)
     setPrdInCart(products)
   }
+  const handleSortNameAtoZ = () => {
+    const products = [...data]
+    const newProducts = products.sort((a, b) => a.name.charCodeAt(0) - b.name.charCodeAt(0));
+    setData(newProducts)
+  }
+  const handleSortNameZtoA = () => {
+    const products = [...data]
+    const newProducts = products.sort((a, b) => b.name.charCodeAt(0) - a.name.charCodeAt(0));
+    setData(newProducts)
+  }
+  const handleSortPriceHighToLow = () => {
+    const products = [...data]
+    const newProducts = products.sort((a, b) => a.price - b.price);
+    setData(newProducts)
+  }
+  const handleSortPriceLowToHigh = () => {
+    const products = [...data]
+    const newProducts = products.sort((a, b) => b.price - a.price);
+    setData(newProducts)
+  }
   return (
     <>
-      <Header prdInCart={prdInCart} removeProduct={removeProduct}/>
+      <Header prdInCart={prdInCart} removeProduct={removeProduct} />
       {/* <ProductList data={dssp} addToCart={addToCart}/> */}
-      <Register />
+      <ProductList 
+        dssp={data} 
+        handleSortNameAtoZ={handleSortNameAtoZ} 
+        handleSortNameZtoA={handleSortNameZtoA} 
+        handleSortPriceHighToLow={handleSortPriceHighToLow} 
+        handleSortPriceLowToHigh={handleSortPriceLowToHigh}
+         />
       <Footer />
     </>
   );
 }
 
-
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,21 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import * as serviceWorker from './serviceWorker';
 import App from './App';
-const dssp = [{
-    Name: 'bàn',
-    Price: 2000,
-    PricePromoted: 1000,
-    Description: 'abc'
-  },{
-    Name: 'bàn',
-    Price: 2000,
-    PricePromoted: 1000,
-    Description: 'abc'
-  },{
-    Name: 'bàn',
-    Price: 2000,
-    PricePromoted: 1000,
-    Description: 'abc'
-  }]
-ReactDOM.render(<App dssp={dssp}/>, document.getElementById('root'));
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
